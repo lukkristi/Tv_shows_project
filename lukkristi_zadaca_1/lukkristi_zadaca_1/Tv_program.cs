c772a25c02c6288fea92abaa0958535ad711430c
ACA_BRUTALNI\Lukas <lukkristi@foi.hr>
2019-10-30 20:15:39 +0100

    CRO: Kreirani atributi, konstruktor i prazna metoda ENG: Added atributs, constructor and a blank method in class Tv_program GER: Atribute, Konstruktor und eine lere Methode sind in der Klasse Tv_program Erstellt

--- lukkristi_zadaca_1/lukkristi_zadaca_1/Tv_program.cs
+++ lukkristi_zadaca_1/lukkristi_zadaca_1/Tv_program.cs
@@ -6,6 +6,21 @@ namespace lukkristi_zadaca_1
 {
     class Tv_program
     {
+        public int ID { get; set; }
+        public List<String> DaniUTjednu { get; set; }
+        public int VrijemePocetka { get; set; }
+        public List<Osoba_uloga> Uloge { get; set; }
 
+        public Tv_program(int id, string dan, int vrijemePocetka)
+        {
+            this.ID = id;
+            //this.Dan = dan;
+            this.VrijemePocetka = vrijemePocetka;
+        }
+
+        public void OdrediDaneUTjednu(string dan)
+        {
+
+        }
     }
 }
