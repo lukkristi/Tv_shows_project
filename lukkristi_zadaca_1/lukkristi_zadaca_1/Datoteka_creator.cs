238428cd2cf63502ab038463bda897194b5e6691
ACA_BRUTALNI\Lukas <lukkristi@foi.hr>
2019-11-03 01:48:41 +0100

    CRO: dodane datoteke sa podacima i pocetak implementacije factory method dizajna ENG: adde .txt files with examples and started implementation for factory method design GER: .txt Dateien hinzugefügt fürs testieren und di implementation der factory Methode begonen

---
+++ lukkristi_zadaca_1/lukkristi_zadaca_1/Datoteka_creator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lukkristi_zadaca_1
+{
+    abstract class Datoteka_creator
+    {
+
+    }
+}
--- lukkristi_zadaca_1/lukkristi_zadaca_1/Program.cs
+++ lukkristi_zadaca_1/lukkristi_zadaca_1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace lukkristi_zadaca_1
 {
@@ -6,7 +9,44 @@ namespace lukkristi_zadaca_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var fullPath = Path.GetFullPath("Neues Textdokument.txt");
+            
+            string[] filePaths = System.IO.Directory.GetFiles(@"..\..", "DZ_1_uloge.txt", SearchOption.AllDirectories);
+            string a = @"\podaci\DZ_1_tvkuca.txt";
+            var fullPath2 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + a;
+            Console.WriteLine(fullPath2);
+            string putanja = Path.GetFullPath(Path.Combine(@"podaci\emisije.txt", @"..\..\..\..\..\..\podaci\"));
+            Console.WriteLine(putanja);
+            string ad = "";
+            try
+            {
+                string[] values= new string[100];
+                List<string> lista = new List<string>();
+                
+                using (var reader = new StreamReader(putanja+ "DZ_1_uloge.txt"))
+                {
+                    reader.ReadLine();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        values = line.Split(';');
+                        Console.WriteLine(values[0] + " ");
+                        lista.Add(values[1]);
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            //string[] files = File.ReadAllLines(path);
+            Console.ReadLine();
         }
     }
 }
